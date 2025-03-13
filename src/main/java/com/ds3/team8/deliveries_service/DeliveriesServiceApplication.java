95d467df140194d92ce9913526b4d3d9f85b6c64
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-03-12 21:46:04 -0500

    Estructura del microservicio

---
+++ src/main/java/com/ds3/team8/deliveries_service/DeliveriesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ds3.team8.deliveries_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveriesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveriesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds3/team8/deliveries_service/DeliveriesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds3.team8.deliveries_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveriesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
