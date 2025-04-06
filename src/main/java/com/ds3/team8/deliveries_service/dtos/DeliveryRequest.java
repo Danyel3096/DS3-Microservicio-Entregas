80e15d4f02ff6c4ea693f13a8d1e5bc439441085
Victor Alzate <victoralzate61@gmail.com>
2025-04-06 15:55:00 -0500

    feat: add validation and fields to DeliveryRequest and DeliveryResponse DTOs
    adding the next code

--- src/main/java/com/ds3/team8/deliveries_service/dtos/DeliveryRequest.java
+++ src/main/java/com/ds3/team8/deliveries_service/dtos/DeliveryRequest.java
@@ -1,4 +1,33 @@
 package com.ds3.team8.deliveries_service.dtos;
 
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DeliveryRequest {
+    @NotEmpty(message = "No puede estar vacio")
+    @Size(min = 2, max = 20, message = "El tamaño tiene que estar entre 2 y 20")
+    private String status;
+
+    @Size(min = 2, max = 20, message = "El tamaño tiene que estar entre 2 y 20")
+    @NotEmpty(message = "No puede estar vacio")
+    private String routeDetails;
+
+    @NotNull(message = "El stock no puede ser nulo")
+    @Min(value = 0, message = "El dealerId no puede ser negativo")
+    private Integer dealerId;
+
+    @NotNull(message = "El stock no puede ser nulo")
+    @Min(value = 0, message = "El orderId no puede ser negativo")
+    private Integer orderId;
 }
--- src/main/java/com/ds3/team8/deliveries_service/dtos/DeliveryResponse.java
+++ src/main/java/com/ds3/team8/deliveries_service/dtos/DeliveryResponse.java
@@ -1,4 +1,19 @@
 package com.ds3.team8.deliveries_service.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DeliveryResponse {
+    private Long id;
+    private String status;
+    private String routeDetails;
+    private Integer dealerId;
+    private Integer orderId;
+
 }
