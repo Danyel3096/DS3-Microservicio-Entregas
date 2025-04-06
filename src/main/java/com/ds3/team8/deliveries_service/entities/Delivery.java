84d6b7eea2c5e78912100cac1225f26439011a99
Sebas <joan.saavedra@correounivalle.edu.co>
2025-04-06 09:06:42 -0500

    feat: adding entititie Delivery
    Se agregó la entidad con sus atributos.

--- src/main/java/com/ds3/team8/deliveries_service/entities/Delivery.java
+++ src/main/java/com/ds3/team8/deliveries_service/entities/Delivery.java
@@ -1,4 +1,29 @@
 package com.ds3.team8.deliveries_service.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "deliveries")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "order_id", nullable = false)
+    private Integer orderId;
+
+    @Column(name = "dealer_id", nullable = false)
+    private Integer dealerId;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(name = "route_details")
+    private String routeDetails;
 }
